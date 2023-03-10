e7f9466e70949dc1f606173a9fc0b92bf43ac13c
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2023-03-10 20:10:45 +0300

    Update models.py

--- learning/models.py
+++ learning/models.py
@@ -14,6 +14,7 @@ class Course(models.Model):
     count_lessons = models.PositiveIntegerField(verbose_name='Кол-во уроков')
 
     class Meta:
+        app_label = 'auth_app'
         verbose_name_plural = 'Курсы'
         verbose_name = 'Курс'
         ordering = ['title']
@@ -35,6 +36,7 @@ class Lesson(models.Model):
     preview = models.TextField(verbose_name='Описание урока', max_length=200)
 
     class Meta:
+        app_label = 'auth_app'
         verbose_name_plural = 'Уроки'
         verbose_name = 'Урок'
         ordering = ['course']
@@ -52,6 +54,7 @@ class Tracking(models.Model):
     passed = models.BooleanField(default=None, verbose_name='Пройден?')
 
     class Meta:
+        app_label = 'auth_app'
         ordering = ['-user']
 
 
